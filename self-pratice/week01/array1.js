a87a23fbe14619342b5e3d7426be567e716febe3
siravijbb <siravijbb@gmail.com>
2025-08-06 10:48:38 +0700

    add:another 2

--- self-pratice/week01/array1.js
+++ self-pratice/week01/array1.js
@@ -1,3 +1,11 @@
-let siravij = [1,'3','5','human',null]
+let siravij = [1,'3','5','human','not null',{maybeoject:"this probably object"},['an','another','array']]
+// array size
 console.log(siravij.length)
+// first item
 console.log(siravij[0])
+// last item
+console.log(siravij[siravij.length-1])
+// storing obj
+console.log(typeof(siravij[siravij.length-2]))
+// storing arr
+console.log(typeof(siravij[siravij.length-1]))
