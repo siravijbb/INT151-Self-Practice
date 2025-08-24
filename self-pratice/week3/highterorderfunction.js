b3ceadb40b9eef0bca5792f50bb13d2b2f88ece3
siravijbb <siravijbb@gmail.com>
2025-08-24 15:24:54 +0700

    pure:wrtitting without tool

--- self-pratice/week3/highterorderfunction.js
+++ self-pratice/week3/highterorderfunction.js
@@ -12,3 +12,26 @@ let filterfunction = numbers.filter(function(number) {
 console.log(filterfunction);
 // It fill ter in array by putting function or calling or methibg inside filter to return only we need [ 2, 4]
 
+let reducefunction = numbers.reduce(function(a, b) {
+    console.log(a,b)
+    return a + b;
+})
+console.log(reducefunction);
+
+// this function reduce an array of thing to only 1 value on return
+
+
+// Get the names of students who passed (score >= 50)
+const students = [
+    { name: "Alice", score: 70 },
+    { name: "Bob", score: 40 },
+    { name: "Charlie", score: 90 },
+];
+let filteredstudents = students.filter(function(student) {
+    return student.score > 50;
+})
+let listofstudent = [] ;
+filteredstudents.filter(function(student) {return listofstudent.push(filteredstudents.name)})
+console.log(filteredstudents);
+console.log(listofstudent);
+// Expected: ["Alice", "Charlie"]
