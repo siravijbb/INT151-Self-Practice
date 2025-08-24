1da5085833d37d724de4a05c0332d7cd00471172
siravijbb <siravijbb@gmail.com>
2025-08-24 15:15:42 +0700

    add:filter

--- self-pratice/week3/highterorderfunction.js
+++ self-pratice/week3/highterorderfunction.js
@@ -4,3 +4,11 @@ let mappedfunction = numbers.map(function(number) {
     return number * 2;
 })
 console.log(mappedfunction);
+// map function do by map things in array then put do function that we defined [ 2, 4, 6, 8, 10 ]
+
+let filterfunction = numbers.filter(function(number) {
+    return number % 2 === 0;
+})
+console.log(filterfunction);
+// It fill ter in array by putting function or calling or methibg inside filter to return only we need [ 2, 4]
+
